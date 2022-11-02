34cb7ed370bb6f33b962371637ed9900eb84a2e0
Christian Garcia <kriz10@outlook.com>
2022-11-01 20:38:37 -0600

    deploy3

--- MayalProject/settings.py
+++ MayalProject/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from django.contrib import staticfiles
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -134,7 +136,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
+
 WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root')
 
 MEDIA_URL = '/media/'
